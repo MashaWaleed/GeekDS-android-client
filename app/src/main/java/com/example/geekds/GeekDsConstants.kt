e139489c1b0ecebb47ce555f781c7d9f8ab77ca7
MashaWaleed <mw2969866@gmail.com>
2026-07-08 12:09:18 +0300

    final stable ads

--- app/src/main/java/com/example/geekds/GeekDsConstants.kt
+++ app/src/main/java/com/example/geekds/GeekDsConstants.kt
@@ -4,7 +4,7 @@ object GeekDsConstants {
     const val TAG = "GeekDS"
     const val PREFS_NAME = "geekds_prefs"
     const val DEFAULT_DEVICE_NAME = "ARC-A-GR-18"
-    const val DEFAULT_CMS_URL = "http://192.168.1.11:5000"
+    const val DEFAULT_CMS_URL = "http://192.168.1.254:5000"
     const val FALLBACK_CMS_URL = "http://192.168.1.212:5000"
     const val RECOVERY_COOLDOWN_MS = 60_000L
 }
